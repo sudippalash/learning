cdb2f57e5adb6516b6e0da7ac5c52067a34dfc10
Sudip Palash <palash.sudip@gmail.com>
2025-08-23 03:55:27 +0600

    u

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('deploy-webhook', function () {
-    if (env('FTP_DEPLOY_SECRET') == request('secret')) {
+    if (env('FTP_DEPLOY_SECRET') != request('secret')) {
+        abort(403, 'Unauthorized');
+    } else {
         Artisan::call('migrate', ['--force' => true]);
         Artisan::call('storage:link');
 
         return 'Artisan run successfully!';
-    } else {
-        return 'Invalid secret!';
     }
 });
 
