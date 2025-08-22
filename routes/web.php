83d6f87a9c0c3305a6d5544a669c66f5a9340c76
Sudip Palash <palash.sudip@gmail.com>
2025-08-23 03:38:11 +0600

    FTP webhook

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
+
+Route::get('deploy-webhook', function () {
+    if (env('FTP_DEPLOY_SECRET') == request('secret')) {
+        Artisan::call('migrate', ['--force' => true]);
+        Artisan::call('storage:link');
+
+        return 'Artisan run successfully!';
+    } else {
+        return 'Invalid secret!';
+    }
+});
 
 Route::get('/', function () {
     return view('welcome');
